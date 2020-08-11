9c8eeef8a447055fed70f0553e34c4c87ca4b8b0
Abdullah Al-Adawi <abdullah.adawi@globitel.com>
2020-08-11 18:49:18 +0300

    Add project files.

---
+++ leave-management/Data/LeaveHistory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class LeaveHistory
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DateRequsted { get; set; }
+        public DateTime DateActoned { get; set; }
+
+        [ForeignKey("RequstingEmployeeId")]
+        public Employee RequstingEmployee { get; set; }
+        public string RequstingEmployeeId { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public bool? Approved { get; set; }
+        [ForeignKey("ApprovedById")]
+        public Employee ApprovedBy { get; set; }
+        public string ApprovedById { get; set; }
+    }
+}
---
+++ leave-management/Models/LeaveHistoryVM.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class LeaveHistoryVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        public DateTime DateRequsted { get; set; }
+        public DateTime DateActoned { get; set; }
+
+      
+        public EmployeeVM RequstingEmployee { get; set; }
+        public string RequstingEmployeeId { get; set; }
+      
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public bool? Approved { get; set; }
+     
+        public EmployeeVM ApprovedBy { get; set; }
+        public string ApprovedById { get; set; }
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
